32b344f6726e691bd190c0c0eb017800ab2574b6
Le Ngoc Tram Anh <letramanh2182005@gmail.com>
2024-09-29 21:46:20 +0700

    feat:home_page

--- demo.js
+++ demo.js
@@ -2,3 +2,21 @@ const handleCount=(a,b)=> {
 return a+b
 }
 console.log(handleCount(2,5))
+
+const profile={
+    fullName:"LE NGOC TRAM ANH",
+    age:19,
+    userId:1,
+    isComplete:false,
+    contact:{
+        email:"letramanh2182005@gmail.com",
+        phone:"0582096427"
+    },
+    address:{
+     city:"HCM",
+     country:"Viet Nam",
+     district:9,
+    }
+
+}
+console.log(profile.contact)
